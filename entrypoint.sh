655f2c09f513a01a2b83aa4645e446f1d396e5a7
scott <scottymacleod110@gmail.com>
2026-05-04 08:32:59 -0400

    Some security improvements across the scope of the application, improvements to the generated report, updated profile information to be used in the report and some quiality of life improvements.

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-# Fix ownership of the persistent /data volume so appuser can write to it.
-# This handles the case where the volume was previously created by a
-# root-running container.  The chown is a no-op when permissions are
-# already correct, so there is no performance penalty on subsequent starts.
-chown -R appuser:appgroup /data
-exec gosu appuser "$@"
